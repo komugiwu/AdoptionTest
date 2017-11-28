ed318ee53ae7fec3ab1f3ade5e72069c4d9adae9
Komugi <komugi@Komugide-MacBook-Pro.local>
2017-11-28 15:31:56 +0800

    move setup JSON data position

--- AdoptionTest/ViewController/ViewController.swift
+++ AdoptionTest/ViewController/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        JSONCoreData().setDatas()
+//        JSONCoreData().setDatas()
     }
     
     //MARK: Activies
